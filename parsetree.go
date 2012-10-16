e9c2d17830b51b1467833effd4b2b0b210e996f9
Brendan Higgins <bjh83@case.edu>
2012-10-16 14:09:07 -0400

    Added opdode for the regex compiler

--- parsetree.go
+++ parsetree.go
@@ -10,14 +10,49 @@ const(
 	Char
 )
 
-type ParseTree struct {
-	Type ttype
-	left, right ParseTree
-	char rune
+type Pipe struct {
+	Left Juxta
+	Right Pipe_
 }
 
-func (p ParseTree) AddLeft(ttype Type) {
-	toAdd := ParseTree{ttype: ttype}
-	p.left = toAdd
+func (p Pipe) Evaluate() list *OpList {
+	list = p.Left.Evaluate()
+	if p.Right != nil {
+		list.Append(p.Right.Evaluate())
+	}
+}
+
+type Pipe_ struct {
+	Left Juxta
+	Right Pipe_
+}
+
+func (p Pipe_) Evaluate() list *OpList {
+
+type Juxta struct {
+	Left Quanta
+	Right Juxta_
+}
+
+type Juxta_ struct {
+	Left Quanta
+	Right Juxta_
+}
+
+type Quanta struct {
+	Left, Right Prim
+}
+
+type Prim struct {
+	Val Evaluater
+	Op Type
+}
+
+type Evaluater interface {
+	Evaluate() OpList
+}
+
+type Char struct {
+	Val rune
 }
 
