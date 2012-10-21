ba244d7b863d5f19a8d506b4313c3a093044bf59
Brendan Higgins <bjh83@case.edu>
2012-10-21 18:58:36 -0400

    I added a test file

---
+++ test/regex-compiler.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+	"compiler"
+	"fmt"
+	"os"
+)
+
+func main(regex string) {
+	instructions := compiler.Compile(regex)
+	file, err = os.Create("test_output.txt")
+	if err != nil {
+		fmt.Println("ERROR: file could not be created")
+	}
+	for index := 0; index < len(instructions); index++ {
+		switch instructions[index].OpCode {
+		case Start:
+			fmt.Fprintf(file, "START\n")
+			break
+		case Char:
+			fmt.Fprintf(file, "CHAR\t", rune(instructions[index].Line1), "\n")
+			break
+		case Jump:
+			fmt.Fprintf(file, "JMP\t", instructions[index].Line1, "\n")
+			break
+		case Split:
+			fmt.Fprintf(file, "SPLIT\t", instructions[index].Line1,
+			instructions[index].Line2, "\n")
+			break
+		case Match:
+			fmt.Fprintf(file, "MATCH")
+			break
+		}
+	}
+	fmt.Println("Compilation complete")
+}
+
